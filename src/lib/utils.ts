1823971a5caa8e7e5c312dc1a2f18b4f920f8e40
WinChin <145524028+Win-Chin@users.noreply.github.com>
2025-02-16 08:26:03 +0800

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,6 @@
-import { clsx } from "clsx";
-import type { ClassValue } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// 导出一个函数，用于合并传入的ClassValue类型的参数
 export function cn(...inputs: ClassValue[]) {
-    // 使用twMerge函数合并传入的参数
     return twMerge(clsx(inputs));
 } 
